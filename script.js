4c6790cbf7619b4d6d91ba305eec2234e9c48b43
weshy007 <josephwaweru96@gmail.com>
2020-12-13 22:55:24 +0300

    new site name

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ var akanFemaleNames = ["Akosua", "Adwoa", " Abenaa", "Akua", "  Yaa", "Afua"];
 var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 function akan() {
 
+    var dd = parseInt(document.getElementById("day").value);
+    var mm = parseInt(document.getElementById("month").value);
     var cc = parseInt(document.getElementById("century").value);
     var yy = parseInt(document.getElementById("year").value);
-    var mm = parseInt(document.getElementById("month").value);
-    var dd = parseInt(document.getElementById("day").value);
+    
     var gender = document.getElementById("gender").value;
     var d = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7
 
